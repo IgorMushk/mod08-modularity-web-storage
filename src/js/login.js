c154d5438faffb72d92cedc14a4e9f98cbdb014f
IgorMushk <igor.mushketik@gmail.com>
2023-07-16 17:27:39 +0300

    add Login to LOgout

--- src/js/login.js
+++ src/js/login.js
@@ -1,3 +1,5 @@
+import storage from './storage';
+
 // ЗАДАЧА 1 (аналогия задачи 3 из ДЗ модуля 8)
 
 // Якщо імейл і пароль користувача збігаються, зберігайте дані з форми при сабмите
@@ -21,18 +23,41 @@ const USER_DATA = {
   email: 'user@mail.com',
   password: 'secret',
 };
-
-const form = document.querySelector('#login-form');
-//console.log(form);
-form.addEventListener('input', onSaveDate);
-
+const LOCAL_KEY = 'login-data';
 const userData = {};
+
+const form = document.querySelector('.login-form');
+//console.log(form);
+const logBtn = document.querySelector('.login-btn');
+const inputs = document.querySelectorAll('.login-input');
+
+form.addEventListener('input', onSaveDate);
+form.addEventListener('submit', clickOnData);
+
+function clickOnData(evt) {
+  evt.preventDefault();
+  const { email, password } = userData;
+  if (!email || !password) {
+    alert('Не все поля заполнены!');
+    return;
+  }
+
+  if (email == !USER_DATA.email || password !== USER_DATA.password) {
+    alert('email или passord не совпадают!');
+    return;
+  }
+  //console.log(userData);
+  storage.save(LOCAL_KEY, userData);
+  logBtn.textContent = 'Logout';
+  inputs.forEach(input => input.setAttribute('readonly', true));
+  form.reset();
+}
 
 function onSaveDate(event) {
   //console.dir(event.target);
-  //console.dir(event.target.name);
-  //console.dir(event.target.value);
+  //   console.dir(event.target.name);
+  //   console.dir(event.target.value);
 
   const { name, value } = event.target;
-  userData[bane] = value;
+  userData[name] = value;
 }
