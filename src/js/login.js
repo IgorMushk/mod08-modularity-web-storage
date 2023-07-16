23c1c175f324146d6f71862dee1298f67969dd15
IgorMushk <igor.mushketik@gmail.com>
2023-07-16 23:24:21 +0300

    Login - logout finish + ToDoList - start

--- src/js/login.js
+++ src/js/login.js
@@ -1,6 +1,8 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import storage from './storage';
 
 // ЗАДАЧА 1 (аналогия задачи 3 из ДЗ модуля 8)
+// https://www.youtube.com/watch?v=zgZ6s4-Y2Wk&t=5493s
 
 // Якщо імейл і пароль користувача збігаються, зберігайте дані з форми при сабмите
 // у локальне сховище і змінюй кнопку login на logout і роби поля введення
@@ -24,33 +26,55 @@ const USER_DATA = {
   password: 'secret',
 };
 const LOCAL_KEY = 'login-data';
-const userData = {};
+let userData = {};
 
 const form = document.querySelector('.login-form');
 //console.log(form);
-const logBtn = document.querySelector('.login-btn');
+const loginBtn = document.querySelector('.login-btn');
 const inputs = document.querySelectorAll('.login-input');
+const todoform = document.querySelector('.todo');
+const saveData = storage.load(LOCAL_KEY);
 
 form.addEventListener('input', onSaveDate);
 form.addEventListener('submit', clickOnData);
 
+// Если данные в локалсторедже есть, делаем все как при submit
+if (saveData) {
+  loginBtn.textContent = 'Logout';
+  inputs.forEach(input => input.setAttribute('readonly', true));
+}
+
 function clickOnData(evt) {
   evt.preventDefault();
+  // Проверка состояния кнопки при клике
+  if (loginBtn.textContent === 'Logout') {
+    loginBtn.textContent = 'Login';
+    //console.log(inputs);
+    inputs.forEach(input => input.removeAttribute('readonly'));
+    storage.remove(LOCAL_KEY);
+    userData = {};
+    todoform.style.display = 'none';
+    return;
+  }
+
   const { email, password } = userData;
   if (!email || !password) {
-    alert('Не все поля заполнены!');
+    //alert('Не все поля заполнены!');
+    Notify.failure('Не все поля заполнены!');
     return;
   }
 
   if (email == !USER_DATA.email || password !== USER_DATA.password) {
-    alert('email или passord не совпадают!');
+    //alert('email или passord не совпадают!');
+    Notify.failure('email или passord не совпадают!');
     return;
   }
   //console.log(userData);
   storage.save(LOCAL_KEY, userData);
-  logBtn.textContent = 'Logout';
+  loginBtn.textContent = 'Logout';
   inputs.forEach(input => input.setAttribute('readonly', true));
   form.reset();
+  todoform.style.display = 'flex';
 }
 
 function onSaveDate(event) {
